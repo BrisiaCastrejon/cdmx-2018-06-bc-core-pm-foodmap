5b6ca5ec62e7c026e3c9702d894f5fa266f4672d
BrisiaCastrejon <brissmonse@gmail.com>
2018-08-06 11:10:59 -0500

    Maquetado de la interfaz sin funcionalidad

---
+++ JS/index.js
@@ -0,0 +1,4 @@
+const init = document.getElementById('init');
+init.addEventListener('click', evento => {
+  location.href = '../views/view1.html';
+})
---
+++ JS/view1.js
@@ -0,0 +1,21 @@
+let elComedor = document.getElementById('elComedor');
+let cercaDeMi = document.getElementById('cercaDeMi');
+let loMasPopular = document.getElementById('loMasPopular');
+let buscar = document.getElementById('buscar');
+let buscador = document.getElementById('buscador');
+
+elComedor.addEventListener('click', event => {
+  alert("el comedor");
+})
+
+cercaDeMi.addEventListener('click', event => {
+  alert("cerca De Mi");
+})
+
+loMasPopular.addEventListener('click', event => {
+  alert("lo mas popular");
+})
+
+buscar.addEventListener('click', event => {
+  alert("buscar");
+})
