26d771f2ae258cf873f42d8e61a2c4cc15ecd198
BrisiaCastrejon <39832639+BrisiaCastrejon@users.noreply.github.com>
2018-08-08 02:43:08 -0500

    Update animacion.js

--- JS/animacion.js
+++ JS/animacion.js
@@ -6,7 +6,7 @@
   let frame = () => {
     if (loading == 100) {
       clearInterval(perrito);
-      window.open("VIEWS/view1.html", "_self");      
+      window.open("../VIEWS/view1.html", "_self");      
     }
     else {
       loading = loading+1;
@@ -25,7 +25,7 @@
   function frame(){
    if(loading == 50) {
     clearInterval(id);
-    window.open("../views/view1.html", "_self");
+    window.open("views/view1.html", "_self");
    }
    else {
     loading = loading + 1;
