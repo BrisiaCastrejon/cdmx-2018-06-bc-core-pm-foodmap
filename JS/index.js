b66a0fdaf4b3010b3e9ad5bed7a7b012d6426379
BrisiaCastrejon <39832639+BrisiaCastrejon@users.noreply.github.com>
2018-08-08 02:43:50 -0500

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,29 @@
-const init = document.getElementById('init');
-init.addEventListener('click', evento => {
-  location.href = '../views/view1.html';
-})
+const url = 'https://raw.githubusercontent.com/BrisiaCastrejon/cdmx-2018-06-bc-core-pm-foodmap/FoodVersion/DATA/restaurantes.json'
+fetch(url)
+  // se asigna la obtencion en formato JSON
+    .then(data => data.json())
+    // se le asigna nombre a la data
+    .then(restaurantes => {
+    console.log(restaurantes);
+    getOptions(restaurantes);
+    getFavorites(restaurantes);
+    getCloser(restaurantes);
+    })
+    .catch(error => {
+    //console.log('Este es un mensaje para reportar error');
+    });
+
+let elComedor = document.getElementById('elComedor');
+let cercaDeMi = document.getElementById('cercaDeMi');
+let loMasPopular = document.getElementById('loMasPopular');
+let buscar = document.getElementById('buscar');
+let buscador = document.getElementById('buscador');
+let home = document.getElementById('home');
+let name;
+let map;
+let address;
+let specialty;
+let score;
+let phone;
+let citykey;
+let reference; 
